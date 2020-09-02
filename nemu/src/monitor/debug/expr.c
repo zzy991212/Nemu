7be6dbec12f4a7ec9f4c6b52d23d3c00127fcc2f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-02 20:49:16 +0800

    >  compile NEMU 3018216336 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:49:16 up  5:00,  1 user,  load average: 0.74, 0.63, 0.57 af8ece0d355c1327b4f9ab065f536f462113fc5f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -77,6 +77,7 @@ static bool make_token(char *e) {
 
 				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
+				position += substr_len;
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
 				 * to record the token in the array `tokens'. For certain types
 				 * of tokens, some extra actions should be performed.
@@ -92,7 +93,6 @@ static bool make_token(char *e) {
 					}
 				}
 
-				position += substr_len;
 
 				break;
 			}
@@ -113,6 +113,8 @@ uint32_t expr(char *e, bool *success) {
 	}
 
 	/* TODO: Insert codes to evaluate the expression. */
+
+
 //	panic("please implement me");
 	*success = true;
 	return 0;
