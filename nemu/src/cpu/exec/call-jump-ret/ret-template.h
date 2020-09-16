770212d0e54846a1d670a0afa6f52f0bfd398e9b
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-16 21:44:51 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:44:51 up 11:33,  1 user,  load average: 0.14, 0.16, 0.17 ccf95d9f9b7521b25723a4247cf9cd98c540aa9d

--- nemu/src/cpu/exec/call-jump-ret/ret-template.h
+++ nemu/src/cpu/exec/call-jump-ret/ret-template.h
@@ -5,10 +5,9 @@
 make_helper(concat(ret_n_, SUFFIX)) {
 
     cpu.eip = MEM_R(reg_l(R_ESP));
-    printf("MEM 0x%x\t\t sw 0x%x\n",MEM_R(reg_l(R_ESP)),swaddr_read(reg_l(R_ESP),4));
     reg_l(R_ESP) += DATA_BYTE;
     print_asm("ret");
-    return 1;
+    return 0;
 } 
 
 #include "cpu/exec/template-end.h"
