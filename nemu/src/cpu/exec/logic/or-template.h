b5bdc325b0a50601f3456be57287c9d678f448ed
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 19:44:17 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:44:17 up 33 min,  1 user,  load average: 0.40, 0.23, 0.19 a844684af38c625362220361102deb185c0682a4

--- nemu/src/cpu/exec/logic/or-template.h
+++ nemu/src/cpu/exec/logic/or-template.h
@@ -3,11 +3,19 @@
 #define instr or
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val | op_src->val;
-	OPERAND_W(op_dest, result);
+	DATA_TYPE ret = op_dest->val | op_src->val;
+	OPERAND_W(op_dest, ret);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	cpu.CF = 0;
+	cpu.OF = 0;
+	cpu.ZF = !ret;
+    cpu.SF = ret >> ((DATA_BYTE << 3) - 1);
+	ret ^= ret >> 4;
+    ret ^= ret >> 2;
+    ret ^= ret >> 1;
+    ret &= 1;
+    cpu.PF = !ret;
 
 	print_asm_template2();
 }
