796100e6d31b0dd86d5b391cdf0e5b44cf792dad
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 02:09:24 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:09:24 up  1:52,  1 user,  load average: 0.34, 0.23, 0.19 743866873f5b6cabd158af039eb8b20b78e4162

--- nemu/src/cpu/exec/call-jump-ret/jge-template.h
+++ nemu/src/cpu/exec/call-jump-ret/jge-template.h
@@ -5,7 +5,7 @@
 static void do_execute () {
 	DATA_TYPE_S imm = op_src -> val;
     print_asm("jge\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.ZF == 1 && cpu.SF == cpu.OF) cpu.eip += imm;
+    if (cpu.ZF == 1 || cpu.SF == cpu.OF) cpu.eip += imm;
 }
 
 make_instr_helper(i)
