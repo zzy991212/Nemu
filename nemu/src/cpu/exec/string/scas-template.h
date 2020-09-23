c2cfb239f0d2c4bc2135ac77a7642dde70496b16
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 16:28:34 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:28:34 up  1:20,  1 user,  load average: 0.31, 0.37, 0.67 c8cb8bdcb947cdf8628173521a22a74105e5f20

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -4,10 +4,8 @@
 
 make_helper(concat(scas_m_, SUFFIX)) {
 
-    DATA_TYPE dest_index = reg_l(R_EDI);   
-    REG(R_EAX) -= dest_index;
-    if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
-    else reg_l(R_EDI) -= DATA_BYTE;
+    if (!reg_l(R_ECX) && cpu.ZF == 0)
+        reg_l(R_EDI) += DATA_BYTE;
 	print_asm("scas");
     return 1;
 }
