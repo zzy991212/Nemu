26866236e91139183ee295bdb8def00e5d524f8e
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 22:42:29 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:42:29 up  3:45,  1 user,  load average: 0.37, 0.43, 0.42 5a3b1a0252a86e7dac699577be7a809d5a33e2db

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -3,19 +3,19 @@
 #define instr scas
 
 make_helper(concat(scas_m_, SUFFIX)) {
-    op_src -> val = (uint32_t)MEM_R(reg_l(R_EDI));
-    op_dest -> val = (uint32_t)REG(R_EAX);
+    uint32_t t2 = MEM_R(reg_l(R_EDI));
+    uint32_t t1 = REG(R_EAX);
 
-    uint32_t ret = op_dest -> val - op_src -> val;
+    uint32_t ret = t1 - t2;
 
     if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
     else reg_l(R_EDI) -= DATA_BYTE;
 	/* TODO: Update EFLAGS. */
     cpu.ZF = !ret;
     cpu.SF = ret >> ((DATA_BYTE << 3) - 1);
-    cpu.CF = (op_dest -> val < op_src -> val);
-    int tmp1 = (op_dest -> val) >> ((DATA_BYTE << 3) - 1);
-    int tmp2 = (op_src -> val) >> ((DATA_BYTE << 3) - 1);
+    cpu.CF = (t1 < t2);
+    int tmp1 = (t1) >> ((DATA_BYTE << 3) - 1);
+    int tmp2 = (t2) >> ((DATA_BYTE << 3) - 1);
     cpu.OF = (tmp1 != tmp2 && tmp2 == cpu.SF);
     ret ^= ret >> 4;
     ret ^= ret >> 2;
