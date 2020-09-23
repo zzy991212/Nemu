5796a0b62a5cf55219f2c3094fc2bea463dbf20f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 09:53:35 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:53:34 up  1:19,  1 user,  load average: 0.77, 0.60, 0.61 c2e63c9d46cc247db9172d43e13d28b955790aa

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -21,13 +21,14 @@ static void do_execute () {
 	print_asm_template2();
 }
 
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
 make_instr_helper(i2a);
 make_instr_helper(i2rm);
 make_instr_helper(r2rm);
 make_instr_helper(rm2r);
 
-#if DATA_BYTE == 2 || DATA_BYTE == 4
-make_instr_helper(si2rm)
-#endif
 
 #include "cpu/exec/template-end.h"
