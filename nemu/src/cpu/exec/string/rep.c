dfde4086d23674da3803b9b035d1f8dd640af6b7
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 22:17:00 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:17:00 up  3:19,  1 user,  load average: 0.55, 0.65, 0.42 7be137b2e5831792fed209b6a04a1db3cf0da97c

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -15,15 +15,15 @@ make_helper(rep) {
 			exec(eip + 1);
 			count ++;
 			cpu.ecx --;
-			// assert(ops_decoded.opcode == 0xa4	// movsb
-			// 	|| ops_decoded.opcode == 0xa5	// movsw
-			// 	|| ops_decoded.opcode == 0xaa	// stosb
-			// 	|| ops_decoded.opcode == 0xab	// stosw
-			// 	|| ops_decoded.opcode == 0xa6	// cmpsb
-			// 	|| ops_decoded.opcode == 0xa7	// cmpsw
-			// 	|| ops_decoded.opcode == 0xae	// scasb
-			// 	|| ops_decoded.opcode == 0xaf	// scasw
-			// 	);
+			assert(ops_decoded.opcode == 0xa4	// movsb
+				|| ops_decoded.opcode == 0xa5	// movsw
+				|| ops_decoded.opcode == 0xaa	// stosb
+				|| ops_decoded.opcode == 0xab	// stosw
+				|| ops_decoded.opcode == 0xa6	// cmpsb
+				|| ops_decoded.opcode == 0xa7	// cmpsw
+				|| ops_decoded.opcode == 0xae	// scasb
+				|| ops_decoded.opcode == 0xaf	// scasw
+				);
 
 			/* TODO: Jump out of the while loop if necessary. */
 			if ((ops_decoded.opcode == 0xa6	
@@ -49,11 +49,11 @@ make_helper(repnz) {
 		exec(eip + 1);
 		count ++;
 		cpu.ecx --;
-		// assert(ops_decoded.opcode == 0xa6	// cmpsb
-		// 		|| ops_decoded.opcode == 0xa7	// cmpsw
-		// 		|| ops_decoded.opcode == 0xae	// scasb
-		// 		|| ops_decoded.opcode == 0xaf	// scasw
-		// 	  );
+		assert(ops_decoded.opcode == 0xa6	// cmpsb
+				|| ops_decoded.opcode == 0xa7	// cmpsw
+				|| ops_decoded.opcode == 0xae	// scasb
+				|| ops_decoded.opcode == 0xaf	// scasw
+			  );
 
 		/* TODO: Jump out of the while loop if necessary. */
 		if ((ops_decoded.opcode == 0xa6	
