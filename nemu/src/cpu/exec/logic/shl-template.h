6d252da58f6e22d643b2569bf95781473bc57eef
3018216336-Zheng Zhiyuan <1978750368@qq.com>
2020-09-24 01:17:26 +0800

    Finish PA2 part2 -> successfully run 3(string) and fixed several bugs

--- nemu/src/cpu/exec/logic/shl-template.h
+++ nemu/src/cpu/exec/logic/shl-template.h
@@ -10,6 +10,16 @@ static void do_execute () {
 	dest <<= count;
 	OPERAND_W(op_dest, dest);
 
+	DATA_TYPE ret = dest;
+	cpu.CF = 0;
+	cpu.OF = 0;
+	cpu.ZF = !ret;
+    cpu.SF = ret >> ((DATA_BYTE << 3) - 1);
+	ret ^= ret >> 4;
+    ret ^= ret >> 2;
+    ret ^= ret >> 1;
+    ret &= 1;
+    cpu.PF = !ret;
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
 	 */
