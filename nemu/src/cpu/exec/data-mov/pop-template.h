be535d14b1140f3816518bd1b0966228442395d0
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 23:14:42 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:14:42 up  4:17,  1 user,  load average: 0.63, 0.44, 0.38 73dee455a3c0a636bbadb17447055bf69983934

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -2,19 +2,18 @@
 
 #define instr pop
 
-#if DATA_BYTE == 2 || DATA_BYTE == 4
+
 static void do_execute() {
     OPERAND_W(op_src,MEM_R(reg_l(R_ESP)));
-    MEM_W(reg_l(R_ESP),0);
+    // MEM_W(reg_l(R_ESP),0);
     reg_l(R_ESP) += DATA_BYTE;
 
     print_asm_template1();
 }
-#endif 
 
-#if DATA_BYTE == 2 || DATA_BYTE == 4
+
 make_instr_helper(r)
 make_instr_helper(rm)
-#endif
+
 
 #include "cpu/exec/template-end.h"
