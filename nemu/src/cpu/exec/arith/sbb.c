250565bbbf701b17909b40ea68a2eb3f9d1074f0
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 10:03:58 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:03:58 up  1:30,  1 user,  load average: 0.66, 0.53, 0.55 4285c3ec2d8cdfb4676411958f94f7d55eb212a

--- nemu/src/cpu/exec/arith/sbb.c
+++ nemu/src/cpu/exec/arith/sbb.c
@@ -14,8 +14,8 @@
 
 /* for instruction encoding overloading */
 
-make_helper_v(sbb_i2a);
-make_helper_v(sbb_i2rm);
-make_helper_v(sbb_si2rm);
-make_helper_v(sbb_r2rm);
-make_helper_v(sbb_rm2r);
+make_helper_v(sbb_i2a)
+make_helper_v(sbb_i2rm)
+make_helper_v(sbb_si2rm)
+make_helper_v(sbb_r2rm)
+make_helper_v(sbb_rm2r)
--- nemu/src/cpu/exec/arith/sub.c
+++ nemu/src/cpu/exec/arith/sub.c
@@ -14,8 +14,8 @@
 
 /* for instruction encoding overloading */
 
-make_helper_v(sub_i2a);
-make_helper_v(sub_i2rm);
-make_helper_v(sub_si2rm);
-make_helper_v(sub_r2rm);
-make_helper_v(sub_rm2r);
+make_helper_v(sub_i2a)
+make_helper_v(sub_i2rm)
+make_helper_v(sub_si2rm)
+make_helper_v(sub_r2rm)
+make_helper_v(sub_rm2r)
