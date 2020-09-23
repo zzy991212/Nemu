2698cfbcaa81f9dac9e7aedd24977f68402c03d5
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 23:17:11 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:17:11 up  4:20,  1 user,  load average: 0.28, 0.37, 0.36 1c8cf0a7e0311d80b2a19585766015549c82ab97

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -4,13 +4,11 @@
 
 make_helper(concat(stos_m_, SUFFIX)) {
     
-    MEM_W(reg_l(R_EDI), REG(R_EAX));
-	if (cpu.DF == 0) reg_l (R_EDI) += DATA_BYTE;
-	else reg_l (R_EDI) -= DATA_BYTE;
-	
-	print_asm("stos%s", str(SUFFIX));
-
-	return 1;
+    MEM_W(reg_l(R_EDI),REG(R_EAX));
+	if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
+	else reg_l(R_EDI) -= DATA_BYTE;
+	print_asm("stos");
+    return 1;
 }
 
 
