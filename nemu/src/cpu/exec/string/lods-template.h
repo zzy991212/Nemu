6a4360716a1b07d135df9ab3b45dbd44ebddab54
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 22:24:00 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:24:00 up  3:26,  1 user,  load average: 0.38, 0.46, 0.42 b772f2e88e27aca76eb1b08c624634f7d13ba32e

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -4,8 +4,8 @@
 
 make_helper(concat(lods_m_, SUFFIX)) {
     REG(R_EAX) = MEM_R(reg_l(R_ESI));
-    if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
-    else reg_l(R_EDI) -= DATA_BYTE;
+    if (cpu.DF == 0) reg_l(R_ESI) += DATA_BYTE;
+    else reg_l(R_ESI) -= DATA_BYTE;
 	print_asm_template2();
     return 1;
 }
