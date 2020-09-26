b48e24c81a04a5c6ac9324453e213eaa7f990281
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 14:56:35 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:56:35 up 13 min,  1 user,  load average: 0.58, 0.45, 0.47 b597aca8d99d5cc53f0804ff7c0381a4b32fed78

--- testcase/src/HDUtest.c
+++ testcase/src/HDUtest.c
@@ -9,27 +9,32 @@ int max(int x,int y){
 	if (x>y) return x;
 	return y;
 }
+int min(int x,int y){
+	if (x<y) return x;
+	return y;
+}
 int main(){
 
 	n = 10, m = 7;
 	s[0]=0;
-	for (int i=1;i<n;i++){
+	int i,j,k;
+	for (i=1;i<n;i++){
 		s[i]=s[i-1]+d[i];
 	}
-	for (int i=0;i<=n;i++)
-		for (int j=0;j<=n;j++)
-			for (int k=0;k<=n;k++)
+	for (i=0;i<=n;i++)
+		for (j=0;j<=n;j++)
+			for (k=0;k<=n;k++)
 				f[i][j][k]=-1e8;
 	f[0][0][0]=0;
 	
-	for (int i=1;i<=n;i++){
+	for (i=1;i<=n;i++){
 		f[i][0][0]=0;
-		for (int j=1;j<=min(i,m);j++){
-			for (int k=0;k<=j;k++)
+		for (j=1;j<=min(i,m);j++){
+			for (k=0;k<=j;k++)
 				f[i][j][k]=max(f[i][j][k],max(f[i-1][j][k],f[i-1][j-1][k]));
-			for (int k=1;k<=j;k++)
+			for (k=1;k<=j;k++)
 			    f[i][j][k]=max(f[i][j][k],f[i-1][j-1][k-1]+d[0]-2*s[i-1]);
-			for (int k=0;k<j;k++)
+			for (k=0;k<j;k++)
 			    f[i][j][k]=max(f[i][j][k],f[i-1][j-1][k+1]+d[0]+2*s[i-1]);
 		}
 	}
