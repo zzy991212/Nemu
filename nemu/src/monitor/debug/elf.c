bbd06421a310e0868ba935e273d53853ccee3d46
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 16:37:54 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:37:54 up  1:55,  1 user,  load average: 0.34, 0.34, 0.34 33f11bb1d126bf4cd9f2048ee3fd9c448d0e5adb

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -84,6 +84,7 @@ uint32_t GetMarkValue(char* str,bool* success){
 	int i;
 	for (i = 0; i < nr_symtab_entry; i++){
 		if ((symtab[i].st_info & 0xf) == STT_OBJECT){
+			printf("%d\n",i);
 			char syb[32];
 			int stlen = symtab[i+1].st_name - symtab[i].st_name - 1;
 			strncpy(syb,strtab + symtab[i].st_name,stlen);
