aac1f43e74a935d14393ca25ec825c8503056c1c
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 14:28:43 +0800

    >  run 3018216336 zzy Linux zzy-virtual-machine 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:28:43 up 12:46,  1 user,  load average: 0.07, 0.33, 0.30 da992e3d3328119eed57807d53e7199aff85b3fd

--- testcase/src/cf1409b.c
+++ testcase/src/cf1409b.c
@@ -1,15 +1,21 @@
 #include "trap.h"
 
-long long a[]={10,12,12343,1000000000,1000000000,10,10};
-long long b[]={10,8,43,1000000000,1000000000,11,11};
-long long x[]={8,8,4543,1,1,2,9};
-long long y[]={5,7,39,1,1,1,1};
-long long n[]={3,2,123212,1,1000000000,5,10};
-long long ans[7];
-long long check[]={70,77,177177,999999999000000000,999999999,55,10};
+long long a[]={10,12,12343,1000000000,10,10};
+long long b[]={10,8,43,1000000000,11,11};
+long long x[]={8,8,4543,1,2,9};
+long long y[]={5,7,39,1,1,1};
+long long n[]={3,2,123212,1000000000,5,10};
+long long ans[6];
+long long check[]={70,77,177177,999999999,55,10};
+
+long long min(long long a,long long b){
+	if (a < b) return a;
+	return b;
+}
 int main(){
-	int T=7;
-	for (int t=0;t<T;t++){
+	int T=6;
+	int t;
+	for (t=0;t<T;t++){
 		long long tmp=a[t]+b[t]-n[t];
 		if (tmp<x[t]+y[t]) {
 			ans[t]=x[t]*y[t];
