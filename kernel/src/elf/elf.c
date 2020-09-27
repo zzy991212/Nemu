96af6c8290c1e4b6a39bf2da21cdcc27a0f831b5
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 21:27:33 +0800

    >  compile kernel 3018216336 zzy Linux zzy-virtual-machine 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:27:33 up  2:37,  1 user,  load average: 0.63, 0.48, 0.42 b07cae4355ffbc4ab442a0704633129c1072dfd0

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,7 +31,7 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x7f454c46;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
