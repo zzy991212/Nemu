bc50737b80a2517f6cf4aa5790e1d2c63b3f9e14
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-13 13:27:28 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:27:28 up  2:51,  1 user,  load average: 1.11, 0.79, 0.43 ac51da75527d260e8901ed5ea3c793647b85dcb9

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -20,7 +20,7 @@ void init_cache(){
     for (i = 0;i < Cache_L1_Size / Cache_L1_Block_Size; i ++) {
         cache1[i].valid = 0;
         cache1[i].tag = 0;
-        memset(cache1[i].data,0,Cache_L1_Block_Size);
+        memset(cache1[i].data,0,sizeof(cache1[i].data));
     }
 }
 
