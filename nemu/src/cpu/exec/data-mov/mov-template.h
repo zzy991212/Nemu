890c356be99fbcfc86bd25951915f8bc346598d7
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-15 21:34:49 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:34:49 up 9 min,  1 user,  load average: 0.20, 0.18, 0.11 33ec038f8d02917021c43a1fce4779070ae7c

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -66,11 +66,12 @@ make_helper(mov_sreg2rm){
 	uint8_t modrm= instr_fetch(eip + 1,1);
 	uint8_t sreg_num = (modrm >> 3) & 7; // reg
 	uint8_t reg_num = modrm & 7; // r/m
-	printf("sreg:%u reg:%d",sreg_num,reg_num);
 	cpu.sreg[sreg_num].selector = reg_w(reg_num);
-	printf("666");
+
 	current_sreg = sreg_num;
+	
 	sreg_load();
+	printf("sreg:%u reg:%d",sreg_num,reg_num);
 	print_asm("mov SREG[%u] %s",sreg_num,REG_NAME(reg_num));
 	return 2;
 }
