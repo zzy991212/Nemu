2f99e3da680d77c4369e5ae5a2cf33776605eae1
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-15 19:23:45 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:23:45 up  2:35,  1 user,  load average: 0.49, 0.39, 0.31 a344f15a7147ad0d1dd17c0e819c59f3508f11a

--- nemu/src/cpu/exec/call-jump-ret/jmp-template.h
+++ nemu/src/cpu/exec/call-jump-ret/jmp-template.h
@@ -32,8 +32,8 @@ make_helper(ljmp){
 
 	swaddr_t chart_addr = cpu.gdtr.base + (idx << 3);//chart addr
     printf("%x\n",chart_addr);
-	sreg_desc -> part1 = instr_fetch(chart_addr, 4);
-	sreg_desc -> part2 = instr_fetch(chart_addr + 4, 4);
+	sreg_desc -> part1 = lnaddr_read(chart_addr, 4);
+	sreg_desc -> part2 = lnaddr_read(chart_addr + 4, 4);
 	Assert(sreg_desc -> p == 1, "Segement Not Exist!");//p bit, whether sreg_desc exists
 
 	cpu.cs.base = 0;
