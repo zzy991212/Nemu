6fed3b7d00ce3c4ff123a7a9758f4919d1963210
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-15 21:48:51 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:48:51 up 21 min,  1 user,  load average: 0.35, 0.22, 0.17 e9711fed23a63a9892919c17ad3eb417bfedb2de

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -57,14 +57,15 @@ void sreg_load(){
 	
 	printf("---%x\n",swaddr_read(cpu.eip+2,1));
 	cpu.sreg[current_sreg].base = 0;
-	cpu.sreg[current_sreg].base |= sreg_desc -> base1;
-	cpu.sreg[current_sreg].base |= sreg_desc -> base2 << 16;
-	cpu.sreg[current_sreg].base |= sreg_desc -> base3 << 24;
-
+	cpu.sreg[current_sreg].base += sreg_desc -> base1;
+	cpu.sreg[current_sreg].base += (sreg_desc -> base2 )<< 16;
+	cpu.sreg[current_sreg].base += (sreg_desc -> base3) << 24;
+	printf("---%x\n",swaddr_read(cpu.eip+2,1));
+	
 	cpu.sreg[current_sreg].limit = 0;
-	cpu.sreg[current_sreg].limit |= sreg_desc -> limit1;
-	cpu.sreg[current_sreg].limit |= sreg_desc -> limit2 << 16;
-	cpu.sreg[current_sreg].limit |= 0xfff << 24;
+	cpu.sreg[current_sreg].limit += sreg_desc -> limit1;
+	cpu.sreg[current_sreg].limit += (sreg_desc -> limit2) << 16;
+	cpu.sreg[current_sreg].limit += (0xfff) << 24;
 	printf("---%x\n",swaddr_read(cpu.eip+2,1));
 	if (sreg_desc -> g == 1) cpu.sreg[current_sreg].limit <<= 12;//G = 0, unit = 1B;G = 1, unit = 4KB
 }
