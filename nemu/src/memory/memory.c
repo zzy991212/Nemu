492628161620ff6ddd1027643c73b048f5dc115f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 00:59:24 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:59:24 up 8 min,  1 user,  load average: 0.34, 0.35, 0.19 f6c35ea9a4def2b320696f70ba6e2d7d7ef760f6

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -22,6 +22,7 @@ lnaddr_t seg_translate(swaddr_t addr,size_t len,uint8_t sreg_id){
 
 hwaddr_t page_translate(lnaddr_t addr){
 	if (cpu.cr0.protect_enable == 1 && cpu.cr0.paging == 1){
+		printf("%x\n",addr);
 		uint32_t dir = addr >> 22;
 		uint32_t page = (addr >> 12) & 0x3ff;
 		uint32_t offset = addr & 0xfff;
