cd4cbe2f5735f305af089bf20cb7ab6c0aaba1f8
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 00:38:31 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:38:31 up  3:47,  1 user,  load average: 0.96, 0.80, 0.55 f010c364b3cb52e844e2f1c892a45c59ab830d01

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -22,15 +22,12 @@ lnaddr_t seg_translate(swaddr_t addr,size_t len,uint8_t sreg_id){
 
 hwaddr_t page_translate(lnaddr_t addr){
 	if (cpu.cr0.protect_enable == 1 && cpu.cr0.paging == 1){
-		printf("%x\n",addr);
 		uint32_t dir = addr >> 22;
 		uint32_t page = (addr >> 12) & 0x3ff;
 		uint32_t offset = addr & 0xfff;
 		// get dir position
 		uint32_t dir_start = cpu.cr3.page_directory_base;
-		printf("%x\n",dir_start);
 		uint32_t dir_pos = (dir_start << 12) + (dir << 2);
-		printf("%x\n",dir_pos);
 		Page_Descriptor first_content;
 		first_content.val = hwaddr_read(dir_pos,4);
 		Assert(first_content.p == 1,"Page Cannot Be Used!");
