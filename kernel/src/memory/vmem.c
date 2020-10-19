8aa291a8bab5ec7a44c5694fb2990f984ea75106
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 02:33:06 +0800

    >  compile kernel 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:33:06 up  1:34,  1 user,  load average: 0.33, 0.34, 0.35 c8b021baf810291477a7ce172c5be3dc6a7f91e0

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -8,14 +8,14 @@
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
 
-extern void add_user_mapping(int id,uint32_t addr);
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	
+
+	extern void add_user_mapping(int id,uint32_t addr);	
 	int tot = SCR_SIZE / PAGE_SIZE;
 	if (SCR_SIZE % PAGE_SIZE != 0) tot ++;
 
