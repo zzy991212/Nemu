d028381719d83620fa95783ec46882fa9f5fb8bc
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 01:49:31 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:49:31 up 54 min,  1 user,  load average: 0.43, 0.49, 0.36 768e5a12f43ea82ec14b3eba365198c874d56cc4

--- kernel/src/memory/mm.c
+++ kernel/src/memory/mm.c
@@ -51,6 +51,7 @@ void add_user_mapping(int id,uint32_t addr){
 	uint32_t dir = addr >> 22;
 	uint32_t page = (addr >> 12) & 0x3ff;
 
-	updir[dir].val = make_pde(va_to_pa(addr));
+	PTE* p = uppage[id];
+	updir[dir].val = make_pde(va_to_pa(p));
 	uppage[id][page].val = make_pte(addr);
 }
