d021751909ac39419d6c387a951b8fd4a9f08c7e
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 02:08:05 +0800

    >  compile kernel 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:08:05 up  1:11,  1 user,  load average: 0.29, 0.43, 0.45 c056c5128ca6a9a8bdf212d8925320331a4855e4

--- kernel/src/memory/mm.c
+++ kernel/src/memory/mm.c
@@ -46,7 +46,7 @@ void init_mm() {
 	ucr3.val = (uint32_t)va_to_pa((uint32_t)updir) & ~0xfff;
 }
 
-static PTE uppage[1][NR_PTE] align_to_page;
+static PTE uppage[1][NR_PTE];
 void add_user_mapping(int id,uint32_t addr){
 	uint32_t dir = (addr >> 22) & 0x3ff;
 	uint32_t page = (addr >> 12) & 0x3ff;
@@ -55,3 +55,5 @@ void add_user_mapping(int id,uint32_t addr){
 	updir[dir].val = make_pde(va_to_pa(p));
 	p[page].val = make_pte(addr);
 }
+// 0xa0000
+// 0000 1100 0000 0000 0000 0000
