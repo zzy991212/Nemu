ead534e2e8ad8e868845dbc65807e80a8509265a
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 22:24:21 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:24:21 up  7:52,  1 user,  load average: 0.66, 0.31, 0.24 23983b70e83697fd6ad43de45df5dc37bf025e5e

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -4,7 +4,9 @@
 
 make_helper(concat(movs_, SUFFIX)) {
     current_sreg = R_DS;
-    swaddr_write(reg_l(R_EDI),DATA_BYTE,MEM_R(reg_l(R_ESI)));
+    uint32_t tmp = MEM_R(reg_l(R_ESI));
+    current_sreg = R_ES;
+    swaddr_write(reg_l(R_EDI),DATA_BYTE,tmp);
 	if (cpu.DF == 0) {
         reg_l(R_EDI) += DATA_BYTE;
         reg_l(R_ESI) += DATA_BYTE;
