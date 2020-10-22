46b35b22c248c81f22763d4ef58673d5ec882c5e
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-22 15:04:46 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:04:46 up  8:57,  1 user,  load average: 0.85, 0.77, 0.62 55a378f8f28ece88916d6a1ea19a7f5ece6b9972

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -64,7 +64,7 @@ hwaddr_t page_translate_additional(lnaddr_t addr,int* flag){
 		uint32_t dir_pos = (dir_start << 12) + (dir << 2);
 		Page_Descriptor first_content;
 		first_content.val = hwaddr_read(dir_pos,4);
-		if (first_content.p == 1) {
+		if (first_content.p == 0) {
 			*flag = 1;
 			return 0;
 		}
@@ -74,7 +74,7 @@ hwaddr_t page_translate_additional(lnaddr_t addr,int* flag){
 		uint32_t page_pos = (page_start << 12) + (page << 2);
 		Page_Descriptor second_content;
 		second_content.val =  hwaddr_read(page_pos,4);
-		if (second_content.p == 1){
+		if (second_content.p == 0){
 			*flag = 2;
 			return 0;
 		}
