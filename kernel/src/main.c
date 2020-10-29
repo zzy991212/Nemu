3c93a7f28eeed5dc0babccd33a609c95e5cd9a38
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 20:21:23 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:21:23 up  6:04,  1 user,  load average: 0.58, 0.39, 0.32 b5c194635c5171ebf36ce252ac7109f39608346f

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -65,6 +65,7 @@ void init_cond() {
 
 #ifdef IA32_PAGE
 	/* Initialize the memory manager. */
+assert(0);
 	init_mm();
 #endif
 
@@ -92,7 +93,7 @@ void init_cond() {
 	/* Clear the test data we just written in the video memory. */
 	video_mapping_clear();
 #endif
-assert(0);
+
 #ifdef IA32_PAGE
 	/* Set the %esp for user program, which is one of the
 	 * convention of the "advanced" runtime environment. */
