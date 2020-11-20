c049d1f470d24176f5163f8352f3b0e94d332d1f
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 21:15:06 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:15:06 up 10:47,  1 user,  load average: 0.85, 0.65, 0.53 f3610d3aa5d65f41ad4d33093bf2f5fa6c1d5be5

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -52,7 +52,7 @@ void init_cond() {
 #ifdef HAS_DEVICE
 	/* Initialize the intel 8259 PIC (Programmable interrupt controller). */
 	init_i8259();
-set_bp();
+
 	/* Initialize the serial port. After that, you can use printk() to output messages. */
 	init_serial();
 	
@@ -63,7 +63,7 @@ set_bp();
 	/* Enable interrupts. */
 	sti();
 #endif
-
+set_bp();
 #ifdef IA32_PAGE
 	/* Initialize the memory manager. */
 	init_mm();
