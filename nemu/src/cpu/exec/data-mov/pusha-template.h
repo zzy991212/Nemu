f861dfb264b64241fee3d954d64800ba705d1afd
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 09:16:14 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:16:14 up  5:16,  1 user,  load average: 0.78, 1.64, 1.62 5babe012b6feab4fa49917745e11417f77484da3

--- nemu/src/cpu/exec/data-mov/pusha-template.h
+++ nemu/src/cpu/exec/data-mov/pusha-template.h
@@ -4,15 +4,15 @@
 
 #if DATA_BYTE == 2
 static void push_w(uint32_t val){
-    REG(R_ESP) -= DATA_BYTE;
-    swaddr_write(REG(R_ESP),DATA_BYTE,val);
+    reg_l(R_ESP) -= DATA_BYTE;
+    swaddr_write(reg_l(R_ESP),DATA_BYTE,(DATA_TYPE)val);
 }
 #endif
 
 #if DATA_BYTE == 4
 static void push_l(uint32_t val){
-    REG(R_ESP) -= DATA_BYTE;
-    swaddr_write(REG(R_ESP),DATA_BYTE,val);
+    reg_l(R_ESP) -= DATA_BYTE;
+    swaddr_write(reg_l(R_ESP),DATA_BYTE,val);
 }
 #endif
 
