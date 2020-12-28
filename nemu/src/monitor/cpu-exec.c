80225064dd68af37c102cd76adc4b7ae8e816d2d
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 22:04:19 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:04:19 up  3:25,  1 user,  load average: 0.52, 0.55, 0.45 fb9a64d7ad7c4d676de153a5b1b4e774c23e596

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -85,7 +85,7 @@ void cpu_exec(volatile uint32_t n) {
 	setjmp(jbuf);
 	int ttt=0;
 	for(; n > 0; n --) {
-		if ((cpu.eax & 0xffff) == 0x8089 && ttt<=100) ttt++,printf("%x\teax:%x\n",cpu.eip,cpu.eax);
+		if ((cpu.esi & 0xffff) == 0x8089 && ttt<=100) ttt++,printf("%x\tesi:%x\n",cpu.eip,cpu.eax);
 		// if (cpu.eip != 0xc0101cea)printf("%x\n",cpu.ebx);
 		// 	else break;
 #ifdef DEBUG
