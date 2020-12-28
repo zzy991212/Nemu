4b47f221a90aed59023fde79d2d4acaf95ea615f
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 20:40:09 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:40:09 up  2:01,  1 user,  load average: 0.53, 1.00, 0.79 7f14c52081c27974b514a68a40dbb0dcdaaf333

--- nemu/src/cpu/exec/call-jump-ret/call-template.h
+++ nemu/src/cpu/exec/call-jump-ret/call-template.h
@@ -3,6 +3,7 @@
 #define instr call
 
 make_helper(concat(call_i_, SUFFIX)) {
+    current_sreg = R_SS;
 	int len = concat(decode_i_, SUFFIX)(cpu.eip + 1);
     reg_l(R_ESP) -= DATA_BYTE;
     swaddr_write(reg_l(R_ESP), 4, cpu.eip + len + 1);
@@ -13,6 +14,7 @@ make_helper(concat(call_i_, SUFFIX)) {
 } 
 
 make_helper(concat(call_rm_, SUFFIX)){
+    current_sreg = R_SS;
     int len = concat(decode_rm_, SUFFIX)(cpu.eip + 1);
 	reg_l(R_ESP) -= DATA_BYTE;
 	swaddr_write(reg_l(R_ESP) , 4, cpu.eip + len + 1);
