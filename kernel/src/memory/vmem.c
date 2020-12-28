7a213ae75234479cb439956eb874ff1b62fdd221
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 19:37:10 +0800

    >  compile kernel 3018216336 zzy Linux zzy-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:37:10 up 58 min,  1 user,  load average: 0.33, 0.31, 0.27 304e9dae10255b3a79d8d962a81cf4a1cee51455

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -7,7 +7,7 @@
 
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
-PTE vm_pt[1][NR_PTE] align_to_page;
+PTE vm_pt[NR_PTE] align_to_page;
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
@@ -15,17 +15,15 @@ void create_video_mapping() {
 	 * some page tables to create this mapping.
 	 */
 
-	PDE *pdir = get_updir();
-	PTE *pt = vm_pt[0];
-	pdir[0].val = make_pde(va_to_pa(pt));
-	int total = SCR_SIZE / PAGE_SIZE + 1;
-	uint32_t addr = VMEM_ADDR;
-	int i = 0;
-	for(; i < total; i++){
-		pt[(addr >> 12) & 0x3ff].val = make_pte(addr);
-		addr += PAGE_SIZE;
-	}
+	PDE *pde = (PDE *)va_to_pa(get_updir());
 
+	pde[0].val = make_pde(va_to_pa(vm_pt));
+	int tot = SCR_SIZE/PAGE_SIZE;
+	if (SCR_SIZE % PAGE_SIZE != 0) tot++;
+	int i;
+	for (i = VMEM_ADDR/PAGE_SIZE;i < VMEM_ADDR/PAGE_SIZE + tot;i ++){
+		vm_pt[i].val = make_pte(i<<12);
+	}
 }
 
 void video_mapping_write_test() {
@@ -33,6 +31,7 @@ void video_mapping_write_test() {
 	uint32_t *buf = (void *)VMEM_ADDR;
 	for(i = 0; i < SCR_SIZE / 4; i ++) {
 		buf[i] = i;
+		Log("$x\n",buf[i]);
 	}
 }
 
