93a3e36776e61219ab2ccba26e035f57ad32ad51
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-29 11:43:42 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:43:42 up  5:58,  1 user,  load average: 0.25, 0.44, 0.55 81c30d4fd791a02d58d0c8183dda712dddeb840

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -114,7 +114,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 
 	int tmp = 0;
 	uint32_t ans = unalign_rw(ret + tmp, 4) & (~0u >> ((4 - len) << 3));
-	printf("%x\t%d\tv:%x\n",addr,(int)len,ans);
+	printf("eip:%x\t%x\t%d\tv:%x\n",cpu.eip,addr,(int)len,ans);
 	uint32_t tmpp = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 	Assert(ans==tmpp,"11111:ans:%x,tmpp:%x",ans,tmpp);
 	// printf("%x\t%x\n",0x7ffff9c,tmpp);
