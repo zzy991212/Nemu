2556caf36503341394de697737da023b8e54cbde
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-29 11:39:20 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:39:20 up  5:54,  1 user,  load average: 0.68, 0.77, 0.67 e670de8a660f3de49202217153557d14b5df95a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -128,8 +128,9 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	if (io_idx != -1){
 		mmio_write(addr,len,data,io_idx);
 	}else {
-		uint32_t ans = hwaddr_read(addr,len);
-		printf("%x\t",ans);
+		// uint32_t ans = hwaddr_read(addr,len);
+		uint32_t tmpp = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+		printf("%x\t",tmpp);
 		write_cache1(addr,len,data);
 		// uint32_t ans = hwaddr_read(addr,len);
 		// uint32_t tmpp = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
