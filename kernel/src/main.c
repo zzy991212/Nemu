a7647969de9c5907fae88e99c3708aec4c580667
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-30 02:05:47 +0800

    >  compile kernel 3018216336 zzy Linux zzy-virtual-machine 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:05:47 up  1:38,  1 user,  load average: 0.76, 0.85, 0.56 7aceb5cbe448ae4bfdae4eac76b1368034ca63d5

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -77,7 +77,7 @@ void init_cond() {
 
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Write some test data to the video memory. */
-	//video_mapping_write_test();
+	video_mapping_write_test();
 	//Log("test");
 #endif
 	// video_mapping_write_test();
@@ -90,9 +90,9 @@ void init_cond() {
 	/* Read data in the video memory to check whether 
 	 * the test data is written sucessfully.
 	 */
-	//video_mapping_read_test();
+	video_mapping_read_test();
 	/* Clear the test data we just written in the video memory. */
-	//video_mapping_clear();
+	video_mapping_clear();
 #endif
 
 #ifdef IA32_PAGE
