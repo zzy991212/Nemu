2b7c4f08afb431cd5827d4333839484e765907b0
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-30 01:50:20 +0800

    >  compile kernel 3018216336 zzy Linux zzy-virtual-machine 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:50:20 up  1:23,  1 user,  load average: 0.24, 0.37, 0.36 4dca997ef1fd29b5f6823c6def0dff521093209e

--- kernel/src/driver/ide/ide.c
+++ kernel/src/driver/ide/ide.c
@@ -16,7 +16,7 @@ void add_irq_handle(int, void (*)(void));
  */
 void ide_read(uint8_t *buf, uint32_t offset, uint32_t len) {
 	uint32_t i;
-	for (i = 0; i < 10; i ++) {
+	for (i = 0; i < len; i ++) {
 		buf[i] = read_byte(offset + i);
 		Log("%d %x",i,buf[i]);
 	}
