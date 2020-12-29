10ba15e0e37576a0b279960c106e32670c805ccd
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-30 00:57:45 +0800

    >  compile NEMU 3018216336 zzy Linux zzy-virtual-machine 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:57:45 up 30 min,  1 user,  load average: 0.68, 0.77, 0.55 a5b45bef5730aeb1df6ab32c5a83eda19d5036d3

--- nemu/src/cpu/exec/data-mov/movzb-template.h
+++ nemu/src/cpu/exec/data-mov/movzb-template.h
@@ -4,24 +4,16 @@
 
 
 static void do_execute() {
-	DATA_TYPE ret = (uint8_t)(op_src -> val) & 0xff;
+	DATA_TYPE ret = (op_src -> val) & 0xff;
+	ret = (uint8_t)ret;
 	OPERAND_W(op_dest, ret);
+	Log_write("movzb:%x\n",ret);
 	print_asm_template2();
 }
 
 
 make_instr_helper(rm2r);
 
-// #if DATA_BYTE != 1
-// make_helper(concat(movzb_rm2r_, SUFFIX)){
-//     uint32_t len = decode_rm_b_internal(eip + 1, op_src, op_dest);
-//     DATA_TYPE res = (uint8_t)op_src->val &0xff;
-//     // res &= 0xff;
-//     concat(write_operand_, SUFFIX)(op_dest, res);
-//     print_asm(str(instr) " %s,%s", op_src->str, op_dest->str);
-//     return len + 1;
-// }
-// #endif
 
 
 #include "cpu/exec/template-end.h"
