d03b69ab6c5692ead3425d7e763577b81d235b39
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-29 23:59:20 +0800

    >  compile kernel 3018216336 zzy Linux zzy-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:59:20 up 14:27,  1 user,  load average: 0.32, 0.38, 0.42 757a01532c4e8ab4108d32221ebc2db84575d41d

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -6,8 +6,28 @@
 #define SCR_SIZE (320 * 200)
 
 /* Use the function to get the start address of user page directory. */
+// PDE* get_updir();
+// PTE vm_pt[NR_PTE] align_to_page;
+// void create_video_mapping() {
+// 	/* TODO: create an identical mapping from virtual memory area 
+// 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
+// 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
+// 	 * some page tables to create this mapping.
+// 	 */
+
+// 	PDE *pde = (PDE *)va_to_pa(get_updir());
+
+// 	pde[0].val = make_pde(va_to_pa(vm_pt));
+// 	int tot = SCR_SIZE/PAGE_SIZE;
+// 	if (SCR_SIZE % PAGE_SIZE != 0) tot++;
+// 	int i;
+// 	for (i = VMEM_ADDR/PAGE_SIZE;i < VMEM_ADDR/PAGE_SIZE + tot;i ++){
+// 		vm_pt[i].val = make_pte(i<<12);
+// 	}
+// }
+#define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE)
 PDE* get_updir();
-PTE vm_pt[NR_PTE] align_to_page;
+PTE my_pt[1][NR_PTE] align_to_page;
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
@@ -15,17 +35,18 @@ void create_video_mapping() {
 	 * some page tables to create this mapping.
 	 */
 
-	PDE *pde = (PDE *)va_to_pa(get_updir());
-
-	pde[0].val = make_pde(va_to_pa(vm_pt));
-	int tot = SCR_SIZE/PAGE_SIZE;
-	if (SCR_SIZE % PAGE_SIZE != 0) tot++;
-	int i;
-	for (i = VMEM_ADDR/PAGE_SIZE;i < VMEM_ADDR/PAGE_SIZE + tot;i ++){
-		vm_pt[i].val = make_pte(i<<12);
+	PDE *pdir = get_updir();
+	PTE *pt = my_pt[0];
+	pdir[0].val = make_pde(va_to_pa(pt));
+	int total = SCR_SIZE / PAGE_SIZE + 1;
+	uint32_t addr = VMEM_ADDR;
+	int i = 0;
+	for(; i < total; i++){
+		pt[(addr >> 12) & 0x3ff].val = make_pte(addr);
+		addr += PAGE_SIZE;
 	}
-}
 
+}
 void video_mapping_write_test() {
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
