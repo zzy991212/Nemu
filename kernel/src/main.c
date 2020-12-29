7bce9b9f7c2873ec3ccf050cfbce374a8fd52825
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-30 02:10:55 +0800

    >  compile kernel 3018216336 zzy Linux zzy-virtual-machine 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:10:55 up  1:43,  1 user,  load average: 0.51, 0.56, 0.51 30d9b8d4a57dd5203ce4b5ceba7c04f16358fe67

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -39,6 +39,10 @@ void init() {
 
 /* Initialization phase 2 */
 void init_cond() {
+	uint32_t i;
+	for (i = 0; i < 28; i ++) {
+		Log("%x",read_byte(i));
+	}
 #ifdef IA32_INTR
 	/* Reset the GDT, since the old GDT in start.S cannot be used in the future. */
 	init_segment();
@@ -48,10 +52,7 @@ void init_cond() {
 	 */
 	init_idt();
 #endif
-uint32_t i;
-	for (i = 0; i < 28; i ++) {
-		Log("%x",read_byte(i));
-	}
+
 #ifdef HAS_DEVICE
 	/* Initialize the intel 8259 PIC (Programmable interrupt controller). */
 	init_i8259();
